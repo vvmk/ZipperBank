f40dac3cdbea1a746fe3f3a3e13e82726297347d
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2018-04-09 08:53:49 -0400

    Checkin

---
+++ README.md
@@ -0,0 +1,2 @@
+# TC-ZCWBank
+[Instructions](https://github.com/Zipcoder/TCUK-Bank-App/blob/master/Instructions.pdf)
---
+++ src/main/java/io/zipcoder/ZcwbankApplication.java
@@ -0,0 +1,12 @@
+package io.zipcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZcwbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZcwbankApplication.class, args);
+	}
+}
