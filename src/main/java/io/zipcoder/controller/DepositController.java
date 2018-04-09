3293b9c23cea5693432f869174d238153da04d65
vvmk <vincentmasiello@gmail.com>
2018-04-09 10:01:15 -0400

    stub depositController

--- src/main/java/io/zipcoder/controller/DepositController.java
+++ src/main/java/io/zipcoder/controller/DepositController.java
@@ -1,10 +1,35 @@
 package io.zipcoder.controller;
 
+import io.zipcoder.domain.Deposit;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * project: zcwbank
  * package: io.zipcoder.controller
  * author: https://github.com/vvmk
  * date: 4/9/18
  */
+@RestController
 public class DepositController {
+
+    public ResponseEntity<Iterable<Deposit>> getAllDepositsByAccountId(Long accountId) {
+        return null;
+    }
+
+    public ResponseEntity<Deposit> getDepositById(Long id) {
+        return null;
+    }
+
+    public ResponseEntity<Deposit> createDeposit(Deposit deposit) {
+        return null;
+    }
+
+    public ResponseEntity<Deposit> updateDeposit(Deposit deposit) {
+        return null;
+    }
+
+    public ResponseEntity deleteDepositById(Long id) {
+        return null;
+    }
 }
