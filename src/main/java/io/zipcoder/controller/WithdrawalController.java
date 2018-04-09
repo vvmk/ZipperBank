d668deb33a31e73e1c93a08be7f4d8032c81e5de
vvmk <vincentmasiello@gmail.com>
2018-04-09 10:04:05 -0400

    Stub WithdrawalController

--- src/main/java/io/zipcoder/controller/WithdrawalController.java
+++ src/main/java/io/zipcoder/controller/WithdrawalController.java
@@ -1,10 +1,35 @@
 package io.zipcoder.controller;
 
+import io.zipcoder.domain.Withdrawal;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * project: zcwbank
  * package: io.zipcoder.controller
  * author: https://github.com/vvmk
  * date: 4/9/18
  */
+@RestController
 public class WithdrawalController {
+
+    public ResponseEntity<Iterable<Withdrawal>> getAllWithdrawalsByAccountId(Long accountId) {
+        return null;
+    }
+
+    public ResponseEntity<Withdrawal> getWithdrawalById(Long id) {
+        return null;
+    }
+
+    public ResponseEntity<Withdrawal> createWithdrawal(Withdrawal withdrawal) {
+        return null;
+    }
+
+    public ResponseEntity<Withdrawal> updateWithdrawal(Withdrawal withdrawal) {
+        return null;
+    }
+
+    public ResponseEntity deleteWithdrawalById(Long id) {
+        return null;
+    }
 }
