7f8c5fc182c96d8761396ac9f854ef3178f96637
vvmk <vincentmasiello@gmail.com>
2018-04-09 11:16:38 -0400

    AccountController mappings, add path vars

--- src/main/java/io/zipcoder/controller/AccountController.java
+++ src/main/java/io/zipcoder/controller/AccountController.java
@@ -4,10 +4,12 @@ import io.zipcoder.domain.Account;
 import io.zipcoder.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 /**
  * project: zcwbank
@@ -18,34 +20,39 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RestController
 public class AccountController {
     private AccountService acctService;
-    
+
     @Autowired
     public AccountController(AccountService accountService) {
         this.acctService = accountService;
     }
 
-    @RequestMapping(value="/accounts",method = GET)
+    @RequestMapping(value = "/accounts", method = GET)
     public ResponseEntity<Iterable<Account>> getAllAccounts() {
         return acctService.getAllAccounts();
     }
 
-    public ResponseEntity<Account> getAccountById(Long id) {
+    @RequestMapping(value = "/accounts/{accountId}", method = GET)
+    public ResponseEntity<Account> getAccountById(@PathVariable Long accountId) {
         return null;
     }
 
-    public ResponseEntity<Iterable<Account>> getAccountsByCustomerId(Long id) {
+    @RequestMapping(value = "/customers/{customerId}/accounts", method = GET)
+    public ResponseEntity<Iterable<Account>> getAccountsByCustomerId(@PathVariable Long customerId) {
         return null;
     }
 
-    public ResponseEntity<Account> createAccount(Account account) {
+    @RequestMapping(value = "/customers/{customerId}/accounts", method = POST)
+    public ResponseEntity<Account> createAccount(@RequestBody Account account, @PathVariable Long customerId) {
         return null;
     }
 
-    public ResponseEntity<Account> updateAccount(Account account) {
+    @RequestMapping(value = "/accounts/{accountId}", method = PUT)
+    public ResponseEntity<Account> updateAccount(@RequestBody Account account, @PathVariable Long accountId) {
         return null;
     }
 
-    public ResponseEntity deleteAccountById(Long id) {
+    @RequestMapping(value = "/accounts/{accountId}", method = DELETE)
+    public ResponseEntity deleteAccountById(@PathVariable Long accountId) {
         return null;
     }
 }
