802a36069abe7e36c8796d03bc3449e3d31d4501
vvmk <vincentmasiello@gmail.com>
2018-04-09 11:34:54 -0400

    DepositController endpoints, param annots

--- src/main/java/io/zipcoder/controller/DepositController.java
+++ src/main/java/io/zipcoder/controller/DepositController.java
@@ -2,7 +2,12 @@ package io.zipcoder.controller;
 
 import io.zipcoder.domain.Deposit;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 /**
  * project: zcwbank
@@ -13,23 +18,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DepositController {
 
-    public ResponseEntity<Iterable<Deposit>> getAllDepositsByAccountId(Long accountId) {
+    @RequestMapping(value = "/accounts/{accountId}/deposits", method = GET)
+    public ResponseEntity<Iterable<Deposit>> getAllDepositsByAccountId(@PathVariable Long accountId) {
         return null;
     }
 
-    public ResponseEntity<Deposit> getDepositById(Long id) {
+    @RequestMapping(value = "/deposits/{depositId}", method = GET)
+    public ResponseEntity<Deposit> getDepositById(@PathVariable Long depositId) {
         return null;
     }
 
-    public ResponseEntity<Deposit> createDeposit(Deposit deposit) {
+    @RequestMapping(value = "/accounts/{accountId}/deposits", method = POST)
+    public ResponseEntity<Deposit> createDeposit(@RequestBody Deposit deposit, @PathVariable Long accountId) {
         return null;
     }
 
-    public ResponseEntity<Deposit> updateDeposit(Deposit deposit) {
+    @RequestMapping(value = "/deposits/{depositId}", method = PUT)
+    public ResponseEntity<Deposit> updateDeposit(@RequestBody Deposit deposit, @PathVariable Long depositId) {
         return null;
     }
 
-    public ResponseEntity deleteDepositById(Long id) {
+    @RequestMapping(value = "/deposits/{depositId}", method = DELETE)
+    public ResponseEntity deleteDepositById(@PathVariable Long depositId) {
         return null;
     }
 }
