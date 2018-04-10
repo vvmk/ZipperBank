580e41d7941925c99420687037bf52495ae6e025
vvmk <vincentmasiello@gmail.com>
2018-04-09 23:04:45 -0400

    Create service test files

---
+++ src/test/java/io/zipcoder/service/implementations/BillServiceImplTest.java
@@ -0,0 +1,13 @@
+package io.zipcoder.service.implementations;
+
+import static org.junit.Assert.*;
+
+/**
+ * project: zcwbank
+ * package: io.zipcoder.service.implementations
+ * author: https://github.com/vvmk
+ * date: 4/9/18
+ */
+public class BillServiceImplTest {
+
+}
