820986df725c3fa0d0a920e79cada9de9d97e92e
vvmk <vincentmasiello@gmail.com>
2018-04-11 21:04:05 -0400

    Add Bill repo query methods.

--- src/main/java/io/zipcoder/repository/BillRepository.java
+++ src/main/java/io/zipcoder/repository/BillRepository.java
@@ -9,5 +9,8 @@ import org.springframework.data.repository.CrudRepository;
  * author: https://github.com/vvmk
  * date: 4/9/18
  */
+
 public interface BillRepository extends CrudRepository<Bill, Long> {
+    Iterable<Bill> findAllByAccount_Id(Long accountId);
+    Iterable<Bill> findAllByAccountCustomer_Id(Long customerId);
 }
