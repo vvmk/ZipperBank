d1930bd5fe287e48ee49c12ac20fee7ee7d41942
vvmk <vincentmasiello@gmail.com>
2018-04-13 16:16:40 -0400

    Write and pass first repo test

--- src/test/java/io/zipcoder/repository/AccountRepositoryTest.java
+++ src/test/java/io/zipcoder/repository/AccountRepositoryTest.java
@@ -1,5 +1,7 @@
 package io.zipcoder.repository;
 
+import io.zipcoder.domain.Account;
+import io.zipcoder.domain.Customer;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +11,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 /**
@@ -29,12 +34,34 @@ public class AccountRepositoryTest {
     @Autowired
     private AccountRepository accountRepo;
 
+    @Autowired
+    private CustomerRepository customerRepo;
+
+    private String nickname = "checking";
+    private final Long customerId = 10L;
+    private final Long accountId = 999L;
+
     @Before
     public void setUp() {
     }
 
     @Test
     public void whenFindAllByCustomer_Id() {
-        
+        // given
+        Customer mockCustomer = new Customer();
+        Long customerId = entityManager.persistAndGetId(mockCustomer, Long.class);
+
+        Account account = new Account();
+        account.setCustomer(mockCustomer);
+        account.setNickname(nickname);
+
+        entityManager.persist(account);
+        entityManager.flush();
+
+        // when
+        Iterable<Account> foundAccounts = accountRepo.findAllByCustomer_Id(customerId);
+
+        // then
+        assertThat(foundAccounts).contains(account);
     }
 }
